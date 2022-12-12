c29351d5df7731933a87a9fd799089d5f28c6777
raghav0477 <sharmaraghav0412@gmail.com>
2022-12-12 17:32:50 +0530

    removed unused code

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ calcDisplayBalance();
 const dep = function () {
   if (inputDeposit.value > 0) {
     expenses.push(Number(inputDeposit.value));
-    // console.log("from inside the matrix", expenses);
+    console.log(expenses);
     balance = expenses.reduce((acc, mov) => acc + mov, 0);
     console.log(balance);
   }
@@ -29,7 +29,7 @@ const dep = function () {
 const wid = function () {
   if (inputWithdraw.value > 0 && inputWithdraw.value <= balance) {
     expenses.push(Number(-inputWithdraw.value));
-    // console.log("from inside the matrix", expenses);
+    console.log(expenses);
     balance = expenses.reduce((acc, mov) => acc + mov, 0);
     console.log(balance);
   }
@@ -72,62 +72,8 @@ btnWithdraw.addEventListener("click", function (eve) {
 trxn_box.insertAdjacentHTML("beforeend", html);
   }
 
-  // if(inputWithdraw.value > 0 && withdrew>=0){
-  // }
   inputWithdraw.value = " ";
 });
-
-// btnDeposit.addEventListener("click", function (eve) {
-//   eve.preventDefault();
-//   // console.log(expenses);
-//   expenses.push(Number(inputDeposit.value));
-//   balance = expenses.reduce((acc, mov) => acc + mov, 0);
-//   console.log(balance);
-//   balance_text.textContent = `₹ ${balance}`;
-//   if(inputDeposit.value <= 0){
-//     alert("Please enter a proper value");
-//   }
-
-//   const html = `
-//       <div class="transaction_row">
-//       <div class="transaction_type" style="color:green;">Deposit</div>
-//         <div class="transaction_value">${Number(inputDeposit.value)}₹</div>
-//       </div>
-//     `;
-//     if(inputDeposit.value > 0){
-
-//       trxn_box.insertAdjacentHTML("beforeend", html);
-//     }
-//   inputDeposit.value = "";
-// });
-
-// btnWithdraw.addEventListener("click", function (eve) {
-//   eve.preventDefault();
-//   if(inputWithdraw.value <= 0){
-//     alert("Invalid Entry");
-//   }
-//   if(inputWithdraw.value <= balance){
-//   withdrew = balance - Number(inputWithdraw.value);
-//   }
-//   // if(inputWithdraw.value <= balance)
-//   balance = withdrew;
-//   console.log(withdrew);
-//   balance_text.textContent = `₹ ${withdrew} `;
-//   if (withdrew < 0) {
-//     alert("Check your expenditures")
-//   }
-//   const html = `
-//   <div class="transaction_row">
-//   <div class="transaction_type" style="color:red;">Withdrawal</div>
-//     <div class="transaction_value">-${Number(inputWithdraw.value)}₹</div>
-//   </div>
-// `;
-
-// if(inputWithdraw.value > 0 && withdrew>=0){
-//   trxn_box.insertAdjacentHTML("beforeend", html);
-// }
-//   inputWithdraw.value = "";
-// });
 
 function showDeposit() {
   if (deposit_box.style.display === "none") {
